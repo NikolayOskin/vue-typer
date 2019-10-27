29b85084d5549f964145936e7aff78862350ab6d
Oskin <wskeal@gmail.com>
2019-10-27 21:39:34 +0300

    adding top menu

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,10 +1,12 @@
 <template>
   <header class="header">
     <div class="header__logo">
-      <img alt="Vue logo" src="./assets/logo.png" class="image" />
+      <img alt="Vue logo" src="../../assets/logo.png" class="image" />
     </div>
     <ul class="menu">
-        <li class="menu__item"><a href="" class="link"></a></li>
+        <li class="menu__item"><router-link to="/" class="link">Home</router-link></li>
+        <li class="menu__item"><router-link to="/login" class="link">Login</router-link></li>
+        <li class="menu__item"><router-link to="/register" class="link">Register</router-link></li>
     </ul>
   </header>
 </template>
@@ -13,6 +15,7 @@
 .header {
   display: flex;
   height: 80px;
+  justify-content: space-between;
 }
 .image {
   max-width: 100%;
@@ -21,5 +24,17 @@
 .header__logo {
   width: 100px;
   padding: 10px;
+}
+.menu {
+  list-style: none;
+  display: flex;
+  align-items: center;
+}
+.menu__item {
+  margin-right: 50px;
+}
+.link {
+  text-decoration: none;
+  color: #2c3e50;
 }
 </style>
