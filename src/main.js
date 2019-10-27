6d8cb08dd31d79bc1443d61123baa2ff57bf2021
Oskin <wskeal@gmail.com>
2019-10-27 22:50:16 +0300

    adding router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Exercises from './components/Exercises'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: Exercises },
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
