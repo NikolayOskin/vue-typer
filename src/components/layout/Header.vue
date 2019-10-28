4ee946af2b8f6d0315cfb61032f07aaa2db3491f
Oskin <wskeal@gmail.com>
2019-10-28 11:05:35 +0300

    exctracting styles from header component to scss import

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
     <div class="header__logo">
-      <img alt="Vue logo" src="../../assets/logo.png" class="image" />
+      <img alt="Vue logo" src="../../assets/images/logo.png" class="image" />
     </div>
     <ul class="menu">
         <li class="menu__item"><router-link to="/" class="link">Home</router-link></li>
@@ -11,30 +11,7 @@
   </header>
 </template>
 
-<style scoped>
-.header {
-  display: flex;
-  height: 80px;
-  justify-content: space-between;
-}
-.image {
-  max-width: 100%;
-  max-height: 100%;
-}
-.header__logo {
-  width: 100px;
-  padding: 10px;
-}
-.menu {
-  list-style: none;
-  display: flex;
-  align-items: center;
-}
-.menu__item {
-  margin-right: 50px;
-}
-.link {
-  text-decoration: none;
-  color: #2c3e50;
-}
+<style lang="scss" scoped>
+@import '@/assets/scss/components/header';
+
 </style>
