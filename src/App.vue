bc80cc292aa3720c87e6fdf75b01af9631cb4036
Oskin <wskeal@gmail.com>
2019-10-28 21:01:43 +0300

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,31 @@
 <template>
-  <div id="app">
-    <Header/>    
+  <div id="app" class="app">
 
-    <Menu @change-exercise="changeExerciseType"/>
+    <Header />
 
-    <Exercise
-      :allowed-keys="allowedKeys"
-      :type-sound="typeSound"
-      :error-sound="errorSound"
-      :backspace-sound="backspaceSound"
-      :selected-exercise="selectedExercise"
-    />
+    <main class="main">
+      <router-view></router-view>
+    </main>
+
+    <Footer />
+
   </div>
 </template>
 
 <script>
-import "@/assets/styles.scss";
-
-import { allowedKeys } from "./core/allowed-keys";
-import { exercises } from "./core/exercises";
-import { sounds } from "./core/sounds";
-
-import Exercise from "./components/Exercise";
-import Menu from "./components/Menu";
-import Header from "./components/layout/Header";
-
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 export default {
-  data() {
-    return {
-      allowedKeys,
-      exercises,
-      selectedExercise: [],
-      ...sounds
-    };
-  },
   name: "app",
   components: {
-    Exercise,
-    Menu
+    Header,
+    Footer
   },
-  methods: {
-    changeExerciseType(type) {
-      this.selectedExercise = this.exercises[type];
-    }
-  }
+
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
+<style lang="scss">
+@import '@/assets/scss/styles';
 </style>
