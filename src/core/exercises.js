efc6855c713d0f881023db85c55ea01746f472c3
Oskin <wskeal@gmail.com>
2019-10-29 00:16:05 +0300

    updating exercises data

--- src/core/exercises.js
+++ src/core/exercises.js
@@ -1,28 +1,69 @@
-export const exercises = {
-    "Text": [
+export const exercises = [
+  {
+    id: 1,
+    name: 'Text exercise 1',
+    category: 'text',
+    content: [
       "This is the first line so yes ",
       "This is the 2nd line of code ",
       "The 3rd line, do you have some more? ",
       "The latest line in this exercise"
-    ],
-    "Numbers": [
+    ]
+  },
+  {
+    id: 2,
+    name: 'Numbers exercise 1',
+    category: 'numbers',
+    content: [
       "47382 7473 0183 01234 8341",
       "3251 920941 757401 384012",
       "234013 75758493 7271 049571",
       "2731 04584387 384710 839301"
-    ],
-    "Symbols": [
+    ]
+  },
+  {
+    id: 3,
+    name: 'Symbols exercise 1',
+    category: 'symbols',
+    content: [
       "!*;№ )*;№ ,.#!*( &*$# (*@)",
       "!$%@ &*)$ *&%^@ ()@#$ &*%$#",
       "$@** &#$% &^#@ (*&# %#@! *(",
       "!!(*% ^$#*@ ()%$#@ @*!*#*("
-    ],
-    "Code": [
+    ]
+  },
+  {
+    id: 4,
+    name: 'Code exercise 1',
+    category: 'code',
+    content: [
       "function isVueAwesome() {",
       "  return true;",
       "}",
       "if (isVueAwesome()) {",
       "  console.log('I like it');",
       "}"
-    ],
+    ]
+  },
+  {
+    id: 5,
+    name: 'Mix exercise 1',
+    category: 'mix',
+    content: [
+      "this is some mixed text 4443",
+      "the 2nd line of mixed text %$@",
+      "3rd line of mixed text 3(**#"
+    ]
+  },
+  {
+    id: 6,
+    name: 'Text exercise 2',
+    category: 'text',
+    content: [
+      "This is the first line of second text ",
+      "Blah blah blah the second line ",
+      "The 3rd line, do you have some more? ",
+      "The latest line in this exercise"
+    ]
   }
+];
