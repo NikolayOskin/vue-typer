b488c1b850791394ffbe5ea8fe758c00a1f9d580
Oskin <wskeal@gmail.com>
2019-10-30 00:25:01 +0300

    components: categories and categoriesList

---
+++ src/components/CategoriesList.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="categories">
+    <li
+      v-for="(category, key) in categories"
+      :key="key"
+      class="categories__item"
+      @click="selectCategory(category)"
+    >
+      <router-link :to="{ name: 'category', params: {categoryName: category } }" class="link">
+        <div class="category-title">{{category}}</div>
+        <div class="category-desc">Exercises: 5</div>
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentCategory: "",
+      categories: ["text", "numbers", "symbols", "code", "mix"]
+    };
+  },
+  methods: {
+    selectCategory(category) {
+      this.$emit("select-category", category);
+      this.currentCategory = category;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/components/categories";
+</style>
+
---
+++ src/views/Category.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="exercise in categoryExercises" :key="exercise.id">
+        <router-link
+          :to="{ name: 'exercise', params: {id: exercise.id } }"
+          class="link"
+        >{{exercise.name}}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { exercises } from "@/core/exercises";
+export default {
+  data() {
+    return {
+      exercises
+    };
+  },
+  computed: {
+    categoryExercises() {
+      return exercises.filter(exercise => {
+        return exercise.category === this.$route.params.categoryName;
+      });
+    }
+  }
+};
+</script>
