644d4440a12696b770118c7246b95a2986a96f3d
Oskin <wskeal@gmail.com>
2019-10-31 00:48:30 +0300

    Categories component

---
+++ src/views/Categories.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    
+    <h2 class="subheader">Categories</h2>
+
+    <CategoriesList />
+
+  </div>
+</template>
+
+<script>
+
+import CategoriesList from "@/components/CategoriesList";
+export default {
+  components: {
+    CategoriesList
+  }
+};
+</script>
