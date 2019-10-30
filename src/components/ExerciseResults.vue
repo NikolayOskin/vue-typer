fd72bee827de099ffcf4c33e6a7d76507a7bca05
Oskin <wskeal@gmail.com>
2019-10-31 00:44:14 +0300

    ExerciseResults dummy component

---
+++ src/components/ExerciseResults.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="results">
+    <h1>Your results</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "ExerciseResults",
+  props: {
+    isFinished: Boolean,
+  }
+};
+</script>
